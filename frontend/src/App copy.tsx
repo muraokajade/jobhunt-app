c6b067ec54469a6b57eacc3a0f7770dcc3f4e4fc
村岡兼通 <muraokakanemichi@Mac.lan>
2026-05-25 20:25:26 +0900

    c

--- frontend/src/App copy.tsx
+++ frontend/src/App copy.tsx
@@ -21,7 +21,7 @@ import {
   buildCompanyRequestBody,
 } from "./utils/companyUtils";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "http://127.0.0.1:8001/api";
 
 type MainPanel = "register" | "list";
 
